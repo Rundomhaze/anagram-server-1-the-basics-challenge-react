6bab21a2d5e20ee8838069678e31d802bac0b606
abakharev <abakharev@sharevault.com>
2022-06-07 15:57:51 +0300

    replace hbs with react

--- views/Words.jsx
+++ views/Words.jsx
@@ -1,12 +1,15 @@
 const React = require('react');
+const Layout = require('./Layout');
 
-module.exports = function Words({ word }) {
+module.exports = function Words({ word, title }) {
   return (
-    <main className="container">
-      <h1>
-        {word}
-      </h1>
-      <p>The anagrams of &quot;{word}&quot; are ...</p>
-    </main>
+    <Layout title={title}>
+      <main className="container">
+        <h1>
+          {word}
+        </h1>
+        <p>The anagrams of &quot;{word}&quot; are ...</p>
+      </main>
+    </Layout>
   );
 };
